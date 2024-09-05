7410c1faafc3d59991884570a516fd0ca6834825
federicomarcheg <84737604+federicomarcheg@users.noreply.github.com>
2024-09-05 15:22:14 +0200

    Add files via upload

---
+++ 11- enchufes web/src/vistas/diseños/12- chat con websockets/14- CRUD EN MONGODB/15- MANGOSTA/02- EXPRESO/src/rutas/16- PRIMERA PRACTICA INTEGRADORA/src/services/17- MONGO AVANZADO - PARTE 1/src/servicios/18- MONGO AVANZADO PARTE 2/src/routers/pets.router.js
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import * as controller from '../controllers/pets.controllers.js';
+
+const router = Router();
+
+router.post('/', controller.createPet);     
+
+router.get('/:id', controller.getByIdPet);
+
+
+export default router;
