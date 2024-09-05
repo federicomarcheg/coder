b7bca045876c5876d7b922ccdebad61ff31650b6
federicomarcheg <84737604+federicomarcheg@users.noreply.github.com>
2024-09-05 15:10:46 +0200

    Add files via upload

---
+++ 33- OPTIMIZACION/01-compreción/src/data.js
@@ -0,0 +1,34 @@
+const data = [
+    {
+      _id: '60908cb30812033def2e7433',
+      index: 0,
+      guid: '818f9a7e-82f0-4816-af73-4b59b27d3cc3',
+      isActive: false,
+      balance: '$1,642.86',
+      picture: 'http://placehold.it/32x32',
+      age: 30,
+      eyeColor: 'blue',
+      name: 'Lopez Pate',
+      gender: 'male',
+      company: 'NITRACYR',
+      email: 'lopezpate@nitracyr.com',
+      phone: '+1 (959) 558-2398',
+      address: '259 Tapscott Street, Marbury, Virgin Islands, 8499',
+      about:
+        'Commodo nostrud dolor aliquip minim nulla deserunt esse exercitation aliqua dolor irure occaecat eiusmod irure. Dolore deserunt dolore id est. Sunt ea dolore ullamco fugiat ex nulla ad commodo consectetur irure laboris fugiat laborum. Incididunt veniam irure sunt tempor aliqua reprehenderit nulla anim reprehenderit.\r\n',
+      registered: '2019-04-20T10:49:50 +03:00',
+      latitude: 20.301768,
+      longitude: 15.555296,
+      tags: ['pariatur', 'non', 'est', 'cupidatat', 'sint', 'incididunt', 'ex'],
+      friends: [
+        {
+          id: 0,
+          name: 'Ramos Gray',
+        },
+        {
+          id: 1,
+          name: 'Charlotte Caldwell',
+        },
+        {
+          id: 2,
+          name: 'Margie Price',
