427d35ced55a8273d5a6a32cf838b81e9cb225fb
federicomarcheg <84737604+federicomarcheg@users.noreply.github.com>
2024-09-05 15:23:21 +0200

    Add files via upload

---
+++ 14- CRUD EN MONGODB/15- MANGOSTA/02- EXPRESO/src/software intermedio/ErrorHandler.js
@@ -0,0 +1,7 @@
+// manejador de errores
+
+export const errorHandler = (error, req, res, next) => {
+    console.log(`error ${error.message}`);
+    const status = error.status || 500;
+    res.status(status).send(error.message);
+  };
