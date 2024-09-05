9abc40e5a3d682d978a4c0b5937b0b2dbc02023d
federicomarcheg <84737604+federicomarcheg@users.noreply.github.com>
2024-09-05 15:15:22 +0200

    Add files via upload

---
+++ 40- DOCUMENTACION EN API REST/02-app-auth/src/persistence/daos/mongodb/product.dao.js
@@ -0,0 +1,8 @@
+import MongoDao from "./mongo.dao.js";
+import { ProductModel } from "./models/products.model.js";
+
+export default class ProductDaoMongo extends MongoDao {
+  constructor() {
+    super(ProductModel);  
+  }
+}
