b58e42d5ce49a1be064bd861b29606eacba3e12a
federicomarcheg <84737604+federicomarcheg@users.noreply.github.com>
2024-09-05 15:00:04 +0200

    Add files via upload

---
+++ src/middlewares/user.validator.js
@@ -0,0 +1,7 @@
+//! SOLICITUD ---> MIDDLEWARE --->ENDPOINT
+
+export const userValidator = (req, res, next) => {
+    const user = req.body
+    if(user.code === 'coder1234') next()
+    else res.status(403).json({msg: 'No autorizado'})
+}
